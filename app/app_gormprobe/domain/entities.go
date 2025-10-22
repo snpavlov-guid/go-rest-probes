d4290954e3be3559cdec7371297c44decc35c61c
sergey pavlov <spavlov@ibs.ru>
2025-10-23 00:28:14 +0300

    update

--- app/app_gormprobe/domain/entities.go
+++ app/app_gormprobe/domain/entities.go
@@ -10,7 +10,7 @@ type Play struct {
 	Name     	string  `gorm:"size:512;index:IDX_PlayName"`
 	Description string  `gorm:"size:1536"`
 	Audience 	int     `gorm:"index:IDX_PlayAudience"`
-	Actors*		[]Actor `gorm:"many2many:PlayActor;"`
+	Actors		[]Actor `gorm:"many2many:PlayActor;"`
 }
 
 // Актер 
@@ -18,7 +18,7 @@ type Actor struct {
 	Id 	     int64   `gorm:"primaryKey;autoIncrement"`
 	Name     string  `gorm:"size:64;index:IDX_ActorName"`
 	Surname  string  `gorm:"size:64;index:IDX_ActorSurname"`
-	Bithdate *time.Time
+	Bithdate time.Time
 }
 
 // Показ
@@ -29,5 +29,5 @@ type Showing struct {
 	Date    time.Time `gorm:"index:IDX_ShowingDate"`
 	Status  byte      `gorm:"index:IDX_ShowingStatus"`
 	Address string    `gorm:"size:1024;index:IDX_ShowingAddress"`
-	Mark*   float32   `gorm:"type:numeric(5,2);index:IDX_ShowingMark"`
+	Mark    float32   `gorm:"type:numeric(5,2);index:IDX_ShowingMark"`
 }
